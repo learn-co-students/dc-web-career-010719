c5fc0ee1e74e3883ff31e180731e73c173624dbd
Paul Nicholsen <paul.nicholsen@flatironschool.com>
2019-01-15 15:57:00 -0500

    post lecture 07

--- 07-intro-to-orms/assignment.md
+++ 07-intro-to-orms/assignment.md
@@ -2,16 +2,32 @@ With a partner/group:
 
 1.  Write the SQL to create tables for Books (id, title, author_id) and Authors (id, name)
 
+CREATE TABLE IF NOT EXISTS books (id INTEGER PRIMARY KEY, title TEXT, author_id INTEGER);
+CREATE TABLE IF NOT EXISTS authors (id INTEGER PRIMARY KEY, name TEXT);
+
 2.  Write the SQL to get all Books
+
+SELECT * FROM books;
 
 3.  Write the SQL to get all Authors
 
+SELECT * FROM authors;
+
 4.  Write the SQL to create a new book, given the title and author id
 
+INSERT INTO books (title, author_id) VALUES ("some_title", 27)
+
 5.  Write the SQL to create a new author, given the author's name
+INSERT INTO authors (name) VALUES ("author's name")
 
 6.  Write the SQL to get all Books by a given author, given the author's id
 
+SELECT * FROM books WHERE author_id = SOME NUMBER
+
 7.  Write the SQL to get all Books by a given author, given the author's name
 
+SELECT * FROM books JOIN authors on authors.id = books.author_id WHERE authors.name = "author name"
+
 8.  Write the SQL to update a Book's title
+
+UPDATE books SET title = new_title WHERE book.id = 27
--- 07-intro-to-orms/lib/author.rb
+++ 07-intro-to-orms/lib/author.rb
@@ -1,3 +1,64 @@
 class Author
 
+    attr_accessor :name
+    attr_reader :id
+
+    def initialize(name, id=nil)
+        @name = name
+        @id = id
+    end
+
+    def save
+        if self.id.nil?
+            sql = <<-SQL
+                INSERT INTO authors (name) VALUES (?)
+            SQL
+            DB.execute(sql, self.name)
+            sql = "SELECT last_insert_rowid()"
+            @id = DB.execute(sql)[0][0]
+        else
+            sql = <<-SQL
+                UPDATE authors SET (name) = ? WHERE id = ?
+            SQL
+            DB.execute(sql, self.name, self.id)
+        end
+        return self
+    end
+
+    def self.make_object_from_row(row)
+        # takes row of data [1, "Beverly Cleary"] and makes Author object
+        id = row[0]
+        name = row[1]
+        Author.new(name, id)
+    end
+
+    def self.find(id)
+        # return the Ruby object with given ID
+        sql = <<-SQL
+            SELECT * from authors where id = ?
+        SQL
+        data = DB.execute(sql, id)
+        self.make_object_from_row(data[0])
+    end
+
+    def self.create(name)
+        author = Author.new(name)
+        author.save
+        author
+    end
+
+    def self.all 
+        sql = "SELECT * from authors"
+        results = DB.execute(sql)
+        results.map do |row|
+            self.make_object_from_row(row)
+        end
+    end
+
+    def delete 
+        sql = <<-SQL
+            DELETE FROM authors WHERE id = ?
+        SQL
+        DB.execute(sql, self.id)
+    end
 end
