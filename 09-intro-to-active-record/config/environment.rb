0de31628de9786eebc2dbaa06153b95905394500
Paul Nicholsen <paul.nicholsen@flatironschool.com>
2019-01-17 15:23:16 -0500

    post lecture

--- 09-intro-to-active-record/config/environment.rb
+++ 09-intro-to-active-record/config/environment.rb
@@ -3,6 +3,7 @@ Bundler.require
 
 # setting up the database connection (old way)
 # DB = SQLite3::Database.new("db/library.db")
+SINATRA_ACTIVESUPPORT_WARNING=false
 
 ActiveRecord::Base.establish_connection(
   adapter: 'sqlite3',
--- 09-intro-to-active-record/db/schema.rb
+++ 09-intro-to-active-record/db/schema.rb
@@ -10,11 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_15_174818) do
+ActiveRecord::Schema.define(version: 2019_01_17_194455) do
 
   create_table "authors", force: :cascade do |t|
     t.string "name"
-    t.string "pen_name"
   end
 
 end
