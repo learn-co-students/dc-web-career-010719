4d1cdbf98ca8ac62421113653dbb607a41d10375
Paul Nicholsen <paul.nicholsen@flatironschool.com>
2019-02-01 09:59:33 -0500

    prelecture 17

--- 15-sinatra-associated-objects/app/controllers/application_controller.rb
+++ 15-sinatra-associated-objects/app/controllers/application_controller.rb
@@ -1,9 +1,10 @@
 class ApplicationController < Sinatra::Base
 
-    set :views, "app/views"
+    # set :views, "app/views/home"
     set :method_override, true
-
-    get "/" do
+    set :root, File.join(File.dirname(__FILE__), '..')
+    set :views, File.join(root, "app/views/home")
+    get "/home" do
         erb :home
     end
 
